d5b8db8c79ae3a40325475a665557406d369cea1
nishitkumar123 <68488770+nishitkumar123@users.noreply.github.com>
2025-04-04 07:09:53 +0530

    Add files via upload

---
+++ helloworld/helloworld/src/main/java/com/project/helloworld/controllers/Controller.java
@@ -0,0 +1,13 @@
+package com.project.helloworld.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello World";
+	}
+}
